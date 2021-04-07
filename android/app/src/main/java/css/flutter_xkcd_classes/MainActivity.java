640cb4eaaaf53722d6bf8330709d7ecf1c382e30
Tom Gibbons <tgibbons@css.edu>
2021-04-07 09:54:55 -0500

    Initial commit - Working version without classes for widgets

---
+++ android/app/src/main/java/css/flutter_xkcd_classes/MainActivity.java
@@ -0,0 +1,6 @@
+package css.flutter_xkcd_classes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/comic.dart
@@ -0,0 +1,8 @@
+class Comic {
+  int num;
+  String img;
+  String title;
+
+  Comic(this.num, this.img, this.title);
+
+}
